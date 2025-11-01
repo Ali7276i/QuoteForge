1a96adab3d734e82c0d8a4feda4a4489157f5f31
Ali7276i <kakjdjiqpfb@gmail.com>
2025-11-01 19:17:47 +0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,16 +1,13 @@
 
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './src/app.component';
-import { FormsModule } from '@angular/forms';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
     provideAnimations(),
-    importProvidersFrom(FormsModule),
   ],
 }).catch((err) => console.error(err));
 
